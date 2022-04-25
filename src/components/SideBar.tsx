ed29967b184d7d35b5e472259abb87831d55b340
leonardo.sepulveda <leomoreira.com@gmail.com>
2022-04-25 09:14:39 -0300

    Done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+interface SideBarProps {
+  genres: {
+    id: number;
+    title: string;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  }[];
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+  selectedGenreId: number;
+}
+export function SideBar(props: SideBarProps) {
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
